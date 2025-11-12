1eb14dd529bcad6174a0d7123d9b774243538060
namyoonseo <ysnam0123@naver.com>
2025-11-12 23:59:42 +0900

    2

--- src/app/components/home/HighLights.tsx
+++ src/app/components/home/HighLights.tsx
@@ -34,7 +34,6 @@ export default function HighLights() {
                   width={video.snippet.thumbnails.medium.width}
                   height={video.snippet.thumbnails.medium.height}
                 />
-                ㅇㅇ
                 <p className="line-clamp-2">{video.snippet.title}</p>
               </a>
             ))}
